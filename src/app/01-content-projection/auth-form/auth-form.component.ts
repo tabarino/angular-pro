227247cd39d3100428acbd2b406772b23076cc94
tabarino <tabarino@outlook.com>
2020-10-08 23:45:45 +0100

    01 - Content Projection

---
+++ src/app/01-content-projection/auth-form/auth-form.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { User } from './auth-form.interface';
+
+@Component({
+    selector: 'auth-form',
+    templateUrl: './auth-form.component.html',
+    styleUrls: ['./auth-form.component.scss']
+})
+export class AuthFormComponent implements OnInit {
+    @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    onSubmit(value: User) {
+        this.submitted.emit(value);
+    }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ContentProjectionComponent } from './01-content-projection/content-projection.component';
+import { AuthFormComponent } from './01-content-projection/auth-form/auth-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ContentProjectionComponent,
+        AuthFormComponent
+    ],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
