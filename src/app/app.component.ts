4e115cf08f64a4b7c9ae223845c3daa267add6ea
tabarino <tabarino@outlook.com>
2020-10-09 21:55:03 +0100

    02 - Projection Slots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { User } from './auth-form/auth-form.interface';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pro';
+    title = 'angular-pro';
+
+    createUser(user: User) {
+        console.log('Create account', user);
+    }
+
+    loginUser(user: User) {
+        console.log('Login', user);
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ContentProjectionComponent } from './01-content-projection/content-projection.component';
-import { AuthFormComponent } from './01-content-projection/auth-form/auth-form.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ContentProjectionComponent,
         AuthFormComponent
     ],
     imports: [
