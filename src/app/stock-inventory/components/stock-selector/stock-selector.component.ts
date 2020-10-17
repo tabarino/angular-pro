92b2fa6e081601d95eea7fa8552982f65c6dbf6e
tabarino <tabarino@outlook.com>
2020-10-17 17:03:45 +0100

    32 - Binding FormControls Select

--- src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Product } from '../../models/product.interface';
 
 @Component({
     selector: 'stock-selector',
@@ -8,6 +9,7 @@ import { FormGroup } from '@angular/forms';
 })
 export class StockSelectorComponent implements OnInit {
     @Input() parent: FormGroup;
+    @Input() products: Product[];
 
     constructor() { }
 
--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { Product } from '../../models/product.interface';
 
 @Component({
     selector: 'stock-inventory',
@@ -18,6 +19,14 @@ export class StockInventoryComponent implements OnInit {
         }),
         stock: new FormArray([])
     });
+
+    product: Product[] = [
+        { id: 1, name: 'Macbook Pro', price: 2800 },
+        { id: 2, name: 'USB-C Adapter', price: 50 },
+        { id: 3, name: 'Ipod', price: 400 },
+        { id: 4, name: 'Iphone', price: 900 },
+        { id: 5, name: 'Apple Watch', price: 600 }
+    ];
 
     constructor() { }
 
